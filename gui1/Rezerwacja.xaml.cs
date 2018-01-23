6a237206d80478edafd855040fa75a29eb2dab17
j p <joanna.podsiadloo@gmail.com>
2018-01-23 23:05:16 +0100

    .

--- gui1/Rezerwacja.xaml.cs
+++ gui1/Rezerwacja.xaml.cs
@@ -83,5 +83,49 @@ namespace gui1
 			
 		}
 
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Button_Click_2(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Button_Click_3(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Button_Click_4(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Button_Click_5(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Button_Click_6(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Button_Click_7(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Button_Click_8(object sender, RoutedEventArgs e)
+		{
+
+		}
 	}
 }
